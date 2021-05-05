a991d4377f59b9fa87b52d238aa0ad1c3aecf8ec
Plan49 <anthonyplanisek1@gmail.com>
2021-05-04 22:11:34 -0400

    added the api to createa meal plan for one day then find the recipe for only the first one in the list

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+
+
+
+//sets meal plan for one day
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=week&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C%20olives",
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C%20olives",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "08eab0a1b7msh583dcfd3bbfa1acp16f8dcjsn0ba008005af3",
@@ -9,6 +13,30 @@ const settings = {
 	}
 };
 
+
+//gets that array
 $.ajax(settings).done(function (response) {
 	console.log(response);
+    id = response.meals[0].id
+
+//finds the ingredients for each recipe
+const settings2 = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + id + "/ingredientWidget.json",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "08eab0a1b7msh583dcfd3bbfa1acp16f8dcjsn0ba008005af3",
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings2).done(function (response) {
+	console.log(response);
 });
+
+
+
+
+});
+
