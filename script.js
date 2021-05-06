d8a4cbdcb6b2204c98884c784c84f6c632926e24
Plan49 <anthonyplanisek1@gmail.com>
2021-05-05 20:28:07 -0400

    added the api for recipe instructions and display recipe name on screen

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+//get 1 day meal day - breakfast, lunch, dinner/name and time to cook -DONE
+//get ingredients - DONE
+//get price
+//get nutrition
+//get recipe instructions
 
 
 //sets meal plan for one day
@@ -14,12 +18,18 @@ const settings = {
 };
 
 
-//gets that array
+//gets meal plan data
 $.ajax(settings).done(function (response) {
-	console.log(response);
+	console.log("meals", response);
     id = response.meals[0].id
 
+$("#recipeName1").text(response.meals[0].title)
+$("#recipeName2").text(response.meals[1].title)
+$("#recipeName3").text(response.meals[2].title)
+
 //finds the ingredients for each recipe
+
+
 const settings2 = {
 	"async": true,
 	"crossDomain": true,
@@ -31,11 +41,27 @@ const settings2 = {
 	}
 };
 
-$.ajax(settings2).done(function (response) {
-	console.log(response);
+//list ingredients
+$.ajax(settings2).done(function (response2) {
+	console.log("ingredients", response2);
 });
 
 
+//get recipe instructions
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + id + "/analyzedInstructions?stepBreakdown=true",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "08eab0a1b7msh583dcfd3bbfa1acp16f8dcjsn0ba008005af3",
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response3) {
+	console.log("instructions", response3);
+});
 
 
 });
