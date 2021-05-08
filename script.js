92dcfb5e0aaf7420f6aa4c83037e76ed3c1d4936
Plan49 <anthonyplanisek1@gmail.com>
2021-05-08 11:57:05 -0400

    changed api links

--- script.js
+++ script.js
@@ -4,12 +4,17 @@
 //get nutrition
 //get recipe instructions
 
+var saved
+
+
+
+//set up a function for this to play after submitting data
 
 //sets meal plan for one day
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C%20olives",
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=week&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C%20olives",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "08eab0a1b7msh583dcfd3bbfa1acp16f8dcjsn0ba008005af3",
@@ -19,22 +24,20 @@ const settings = {
 
 
 //gets meal plan data
-$.ajax(settings).done(function (response) {
+$.ajax(settings).done(function recipe(response) {
 	console.log("meals", response);
-    id1 = response.meals[0].id
-    id2 = response.meals[1].id
-    id3 = response.meals[2].id
-
-$("#recipeName1").text(response.meals[0].title)
-$("#recipeName2").text(response.meals[1].title)
-$("#recipeName3").text(response.meals[2].title)
-
+    id1 = JSON.parse(response.items[0].value)
+    id2 = JSON.parse(response.items[1].value)
+    id3 = JSON.parse(response.items[2].value)
+$("#recipeName1").text(id1.title)
+$("#recipeName2").text(id2.title)
+$("#recipeName3").text(id3.title)
 
 //get recipe instructions and ingredients
 const settings3 = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk?ids=" + id1 + "%2C" + id2 + "%2C" + id3,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk?ids=" + id1.id + "%2C" + id2.id + "%2C" + id3.id,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "08eab0a1b7msh583dcfd3bbfa1acp16f8dcjsn0ba008005af3",
@@ -68,6 +71,7 @@ $("#foodImage3").attr("src", response3[2].image)
         $("#instructions3").append(response3[2].analyzedInstructions[0].steps[i].step + "<br>")
         
     }
+
     //lists each ingredient
     for (let i = 0; i < response3[0].extendedIngredients.length; i++) {
         var img = document.createElement("img")
@@ -86,17 +90,18 @@ $("#foodImage3").attr("src", response3[2].image)
         img.src = "https://spoonacular.com/cdn/ingredients_100x100/" + response3[2].extendedIngredients[i].image
         document.getElementById("ingredients3").appendChild(img)
     }
-
-    $("#readyInMinutes1").text("Ready In: " + response.meals[0].readyInMinutes + " Minutes")
-    $("#readyInMinutes2").text("Ready In: " + response.meals[1].readyInMinutes + " Minutes")
-    $("#readyInMinutes3").text("Ready In: " + response.meals[2].readyInMinutes + " Minutes")
-    $("#servings1").text("Serving Size: " + response.meals[0].servings)
-    $("#servings2").text("Serving Size: " + response.meals[1].servings)
-    $("#servings3").text("Serving Size: " + response.meals[2].servings)
+    //ready in minutes/servings
+    $("#readyInMinutes1").text("Ready In: " + response3[0].readyInMinutes + " Minutes")
+    $("#readyInMinutes2").text("Ready In: " + response3[1].readyInMinutes + " Minutes")
+    $("#readyInMinutes3").text("Ready In: " + response3[2].readyInMinutes + " Minutes")
+    $("#servings1").text("Serving Size: " + response3[0].servings)
+    $("#servings2").text("Serving Size: " + response3[0].servings)
+    $("#servings3").text("Serving Size: " + response3[0].servings)
 
 });
 
 //recipe nutrition - ignore
+/*
 const settings4 = {
 	"async": true,
 	"crossDomain": true,
@@ -112,9 +117,34 @@ $.ajax(settings4).done(function (response4) {
 	console.log("nutrition", response4);
 });
 
-
+*/
 
 
 
 });
 
+console.log("days in month", moment("2021-06", "YYYY-MM").daysInMonth())
+console.log("day of the month", moment().date())
+console.log("current date", moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+/*
+$("#save").click(function data(){
+
+    var storage = JSON.parse(localStorage.getItem('cityName')) || []
+    
+    storage.push(city.val())
+
+   localStorage.setItem("cityName", JSON.stringify(storage))
+    
+   saved = JSON.parse(localStorage.getItem("cityName"))
+
+        })
+        //retrives data
+        function checkLocalStorage() {
+           var searches = JSON.parse(localStorage.getItem('cityName'))
+
+           // make the buttons and put them on the page
+           
+        }
+
+        checkLocalStorage()
+        */
