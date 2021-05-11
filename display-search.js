e15872435dc2e4eb3c74664a24e2293bcc847394
Lesley Moore <ldmoore15@gmail.com>
2021-05-11 19:29:04 -0400

    conflicts

--- display-search.js
+++
@@ -1,4 +0,0 @@
-"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C%20olives",
-Calories=2000
-diet=vegetarian
-exclude=shellfish
